251f840dbd750e19668fc83b8d07f8b1b8c03ac8
WoshiWoshu <58432702+WoshiWoshu@users.noreply.github.com>
2020-01-09 20:51:02 +0100

    Added files

---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2018
+** libmy
+** File description:
+** libmy
+*/
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef READ_SIZE
+#define READ_SIZE 1
+#endif /* !READ_SIZE */
+
+char *get_next_line(int);
+
+#endif /* !GET_NEXT_LINE_H */
